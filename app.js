83788f6274ffcdb9bafc7220e43368aec55d244a
tech-souvik <officialsouvikmaji@gmail.com>
2024-02-09 19:18:35 +0530

    Igniting Our App

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-const heading1 = React.createElement("h1", { id: "heading1" }, "Heading 1");
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const heading2 = React.createElement("h2", { id: "heading2" }, "Heading 2");
+const heading1 = React.createElement("h1", { id: "Hello World" }, "Heading 1");
+
+const heading2 = React.createElement(
+  "h2",
+  { id: "Heading by Percel" },
+  "Heading 2"
+);
 
 const container = React.createElement("div", { id: "container" }, [
   heading1,
